e10270be6810798475de8468969343249655077b
Max Lätzsch <68281001+MakkuLeafos@users.noreply.github.com>
2024-06-05 00:37:33 +0200

    Created ToDoElement Wrapper Class, updated gitignore

---
+++ ToDoWebApp.Server/Classes/ToDoElement.cs
@@ -0,0 +1,89 @@
+﻿namespace ToDoWebApp.Server.Classes
+{
+    public class ToDoElement
+    {
+        private int toDoId;
+        private string toDoTitle;
+        private string toDoDescription;
+        private DateTime? toDoDueDate;
+
+        /// <summary>
+        /// Generates an ToDoElement Object (Constructor)
+        /// </summary>
+        /// <param name="id">ID of the ToDoElement</param>
+        /// <param name="title">Title of the ToDoElement</param>
+        /// <param name="description"></param>
+        /// <param name="dueDate"></param>
+        public ToDoElement(int id, string title, string description = "", DateTime? dueDate = null)
+        {
+            toDoId = id;
+            toDoTitle = title;
+            toDoDescription = description;
+            toDoDueDate = dueDate;
+        }
+
+        /// <summary>
+        /// Gets the ID of the ToDo task
+        /// </summary>
+        /// <returns>ToDo task ID</returns>
+        public int GetId()
+        {
+            return toDoId;
+        }
+
+        /// <summary>
+        /// Gets the title of the ToDo task
+        /// </summary>
+        /// <returns>ToDo task title</returns>
+        public string GetTitle()
+        {
+            return toDoTitle;
+        }
+
+        /// <summary>
+        /// Sets the title of the ToDo task
+        /// </summary>
+        /// <param name="title">Setting value for the ToDo task title</param>
+        public void SetTitle(string title)
+        {
+            toDoTitle = title;
+        }
+
+        /// <summary>
+        /// Gets the descritption of the ToDo task
+        /// </summary>
+        /// <returns>ToDo task description</returns>
+        public string GetDescription()
+        {
+            return toDoDescription;
+        }
+
+        /// <summary>
+        /// Sets the description of the ToDo task
+        /// </summary>
+        /// <param name="description">Setting value for the ToDo task description</param>
+        public void SetDescription(string description)
+        {
+            toDoDescription = description;
+        }
+
+        /// <summary>
+        /// Gets the due date of the ToDo task
+        /// </summary>
+        /// <returns>ToDo task due date</returns>
+        public DateTime? GetDueDate() 
+        {
+            return toDoDueDate;
+        }
+
+        /// <summary>
+        /// Sets the due date of the ToDo task
+        /// </summary>
+        /// <param name="dueDate">Setting value for the ToDo task due date</param>
+        public void SetDueDate(DateTime dueDate)
+        { 
+            toDoDueDate=dueDate;
+        }
+
+    }
+}
--- ToDoWebApp.Server/obj/Debug/net8.0/ToDoWebApp.Server.AssemblyInfo.cs
+++ ToDoWebApp.Server/obj/Debug/net8.0/ToDoWebApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoWebApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb586712e5db767565d5c1dd02cbc3af64891b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0c7b3ac53724379f9e8aad27be0cbbe952b8af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoWebApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoWebApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
